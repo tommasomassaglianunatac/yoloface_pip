92db6da70033d2ab5a629c596b1565cc0bc0e547
Tommaso Massaglia <105795468+tommasomassaglianunatac@users.noreply.github.com>
2022-06-01 18:30:59 +0200

    updated some things

--- setup.py
+++ setup.py
@@ -1,23 +1,53 @@
-import setuptools
+import setuptools, os
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+PACKAGE_NAME = 'yolov5-face'
+VERSION = '1.0.0'
+AUTHOR = 'Rebrikov Artem'
+EMAIL = 'https://github.com/elyha7'
+DESCRIPTION = 'The project is a wrap over yolov5-face repo. Made simple portable interface for model import and inference.'
+GITHUB_URL = 'https://github.com/tommasomassaglianunatac/yoloface_pip'
+
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+import_name = os.path.basename(parent_dir)
+
+with open('{}/README.md'.format(parent_dir), 'r') as f:
+    long_description = f.read()
 
 setuptools.setup(
-    name='yolov5face_wrapped',
-    version='0.1',
-    author='Rebrikov Artem',
-    author_email='https://github.com/elyha7',
-    description='pip-installable yoloface wrapper',
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/tommasomassaglianunatac/yoloface_pip',
-    project_urls = {
-        "git page": 'https://github.com/tommasomassaglianunatac/yoloface_pip'
-    },
-    license='MIT',
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
-    include_package_data=True
+    long_description_content_type='text/markdown',
+    url=GITHUB_URL,
+    packages=[
+        'face_detector',
+        'face_detector.models',
+        'face_detector.utils',
+    ],
+    package_dir={'face_detector':'.'},
+    package_data={'weights': ['*.pt'],'models': ['*.yaml']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["thop",
+                    "scipy",
+                    "seaborn",
+                    "joblib",
+                    "matplotlib",
+                    "torch",
+                    "pandas",
+                    "opencv_python",
+                    "tqdm",
+                    "numpy",
+                    "torchvision",
+                    "requests",
+                    "Pillow",
+                    "PyYAml",
+                    "wandb"
+                    ],
 )
